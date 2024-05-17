bb0a8c65ed0f4de60e9e4272095d101bc3a9116e
abrahanalarcon <moiseshazim@gmail.com>
2024-05-17 07:27:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using ConsultasPacientes004.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ConsultasPacientesContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("conexion"));
+
+
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+}
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("NuevaPolitica");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
